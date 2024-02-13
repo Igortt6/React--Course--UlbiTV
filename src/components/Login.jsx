e275de5f67766bf33d712fd630472a5f57f3a81d
Ihor Tkachenko <Igor.tt6@gmail.com>
2024-02-13 15:16:46 +0200

    readme first uppdate

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,25 @@
-import { useContext } from "react";
-import MainButton from "./UI/button/MainButton";
-import MainInput from "./UI/input/MainInput";
-import { AuthorContext } from "../context";
+import { useContext } from 'react'
+import { AuthorContext } from '../context'
+import MainButton from './UI/button/MainButton'
+import MainInput from './UI/input/MainInput'
 
 const Login = () => {
-    const { isAuthor, setIsAuthor } = useContext(AuthorContext)
-    const login = e => {
-        e.preventDefault();
-        setIsAuthor(true);
-        localStorage.setItem('author', 'true')
-    }
+	const { isAuthor, setIsAuthor } = useContext(AuthorContext)
+	const login = e => {
+		e.preventDefault()
+		setIsAuthor(true)
+		localStorage.setItem('author', 'true')
+	}
 
-    return (
-        <div>
-
-            <h1>Сторінка для логіна</h1>
-            <form onSubmit={login}>
-                <MainInput type="text" placeholder="Введіьт логін" />
-                <MainInput type="text" placeholder="Введіть пароль" />
-                <MainButton>Увійти</MainButton>
-            </form>
-
-        </div>
-    )
+	return (
+		<div>
+			<h1>Сторінка для логіна</h1>
+			<form onSubmit={login}>
+				<MainInput type='text' placeholder='Введіть логін' />
+				<MainInput type='text' placeholder='Введіть пароль' />
+				<MainButton>Увійти</MainButton>
+			</form>
+		</div>
+	)
 }
-export default Login;   
+export default Login
